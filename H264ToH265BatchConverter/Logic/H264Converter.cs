018b115ff0ba70895d4a5b2db662022ed9fa185f
Jordan Olivet <jordanolivet@gmail.com>
2022-07-25 00:41:40 +0200

    v0

---
+++ H264ToH265BatchConverter/Logic/H264Converter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+
+namespace H264ToH265BatchConverter.Logic
+{
+    public static class H264Converter
+    {
+        public static Action<string> Logger { get; set; }
+
+        public static void ToH265Async(string input, string output)
+        {
+            BackgroundWorker wk = new();
+            wk.DoWork += (s ,e)=>
+            {
+                if (ToH265(input, output, false))
+                {
+
+                }
+                else
+                {
+
+                }
+            };
+
+            wk.RunWorkerCompleted += (s, e) => 
+            {
+
+            };
+        }
+
+        public static bool ToH265(string input, string output, bool logOn = true)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = @".\ffmpeg\ffmpeg.exe";
+            proc.StartInfo.Arguments = $@"-hwaccel cuda -hwaccel_device 0 -hwaccel_output_format cuda -v verbose -i ""{input}"" -c:v hevc_nvenc -gpu:v 0 -preset llhp -rc:v cbr -c:a copy ""{output}""";
+
+            //if (logOn) { Logger?.Invoke("Command line : " + proc.StartInfo.Arguments + Environment.NewLine); }
+
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+
+            if (!proc.Start())
+            {
+                if (logOn) { Logger?.Invoke("Starting failed"); }
+                return false;
+            }
+            StreamReader reader = proc.StandardError;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (logOn) { Logger?.Invoke(line); }
+            }
+
+            int exitCode = proc.ExitCode;
+
+            proc.Close();
+
+            if (exitCode == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
--- H264ToH265BatchConverter/MainWindow.xaml.cs
+++ H264ToH265BatchConverter/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using H264ToH265BatchConverter.Logic;
+using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace H264ToH265BatchConverter
 {
@@ -20,9 +14,138 @@ namespace H264ToH265BatchConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        double TotalMinutes = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            H264Converter.Logger += (log) =>
+            {
+                tbLogs.Dispatcher.Invoke(() => { tbLogs.Text += log + Environment.NewLine;  });
+            };
+        }
+
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            string input;
+            string output;
+
+            //OpenFileDialog openFileDialog = new();
+            //openFileDialog.Title = "Chosse file to convert";
+            //openFileDialog.ShowDialog(this);
+
+            //input = openFileDialog.FileName;
+
+            //SaveFileDialog saveFileDialog = new();
+            //saveFileDialog.Title = "Chosse destination file";
+            //saveFileDialog.ShowDialog(this);
+
+            //output = saveFileDialog.FileName;
+
+            input = @"C:\Users\Lakio\Desktop\test_video_h264.mp4";
+            output = @"C:\Users\Lakio\Desktop\test_video_h265.mp4";
+
+            if (string.IsNullOrWhiteSpace(input) || string.IsNullOrWhiteSpace(output))
+            {
+                System.Windows.MessageBox.Show("At least one of the two files path is empty, please select correct path.");
+                return;
+            }
+
+            tbLogs.Clear();
+
+            H264Converter.ToH265(input, output);
+
+            //if (!H264Converter.ToH265(input, output))
+            //{
+            //    MessageBox.Show("Convertion failed");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Convertion succeed");
+            //}
+        }
+
+        private void btnConvertMulti_Click(object sender, RoutedEventArgs e)
+        {
+            string inputFolder;
+
+            FolderBrowserDialog openFolderDialog = new();
+            openFolderDialog.ShowDialog();
+
+            inputFolder = openFolderDialog.SelectedPath;
+
+            if(Directory.Exists(inputFolder))
+            {
+                DirectoryInfo dir = new(inputFolder);
+
+                ConvertInBackground(dir);
+            }
+        }
+
+        private void ConvertInBackground(DirectoryInfo dir)
+        {
+            BackgroundWorker wk = new()
+            {
+                WorkerReportsProgress = true
+            };
+
+            wk.DoWork += (s, e) =>
+            {
+                Stopwatch watch = new();
+                watch.Start();
+
+                ConvertDirectory(dir, wk);
+
+                watch.Stop();
+                TotalMinutes = watch.Elapsed.TotalMinutes;
+            };
+
+            wk.ProgressChanged += (p, o) =>
+            {
+                tbLogs.Dispatcher.Invoke(() => { tbLogs.Text += o.UserState + Environment.NewLine; tbLogs.ScrollToEnd(); });
+            };
+
+            wk.RunWorkerCompleted += (s, e) =>
+            {
+                tbLogs.Text += "Total time in minutes : " + TotalMinutes + Environment.NewLine;
+            };
+
+            wk.RunWorkerAsync();
+        }
+
+        private void ConvertDirectory(DirectoryInfo dir, BackgroundWorker wk = null)
+        {
+            if (dir != null)
+            {
+                var dirs = dir.EnumerateDirectories();
+                var files = dir.EnumerateFiles();
+
+                foreach (var f in files)
+                {
+                    if (f.Extension.ToLower() != ".mp4" && f.Extension.ToLower() != ".mkv") { continue; }
+                    
+                    string input = f.FullName;
+                    string output = f.FullName.Replace(f.Extension, string.Empty) + "_h265" + f.Extension;
+
+                    if (H264Converter.ToH265(input, output, false))
+                    {
+                        if (wk != null)
+                        {
+                            wk.ReportProgress(0, f.FullName + " converted");
+                        }
+                        string tmp = f.FullName;
+                        f.Delete();
+                        FileInfo fOutput = new(output);
+                        fOutput.MoveTo(tmp);
+                    }
+                }
+
+                foreach (var d in dirs)
+                {
+                    ConvertDirectory(d, wk);
+                }
+            }
         }
     }
 }
